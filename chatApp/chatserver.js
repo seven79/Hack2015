ec06b1072caba28b3f5eaa64d27b565899585ac2
seven79 <jingxiongh@wl-10-190-86-144.wireless.duke.edu>
2015-11-07 21:00:25 -0500

    Added ChatAPP

---
+++ chatApp/chatserver.js
@@ -0,0 +1,71 @@
+/* 
+Source file: chatserver.js
+
+---
+Copyright (c) 2012 All Right Reserved, C. A. Cois
+http://www.codehenge.net
+
+THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY 
+KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+PARTICULAR PURPOSE.
+
+Node.js is an official trademark of Joyent. This software is not 
+formally related to or endorsed by the official Joyent Node.js open 
+source or commercial project.
+*/
+
+var http = require('http'),
+url = require('url'),
+fs = require('fs');
+
+var server = http.createServer(function (req, res) {
+
+	var path = url.parse(req.url).pathname; 
+	
+    switch (path){
+	case '/':
+		fs.readFile(__dirname + '/chatclient.html', function(err, data){
+		if (err) return send404(res);
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(data, 'utf8');
+		res.end();
+	});
+	break;
+							   
+	default: send404(res);
+	}
+}),
+
+send404 = function(res){	
+	res.writeHead(404);	
+	res.write('404');	
+	res.end();	
+};
+
+server.listen(8080, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:8080/');
+
+var io = require('socket.io').listen(server);
+
+// on a 'connection' event
+io.sockets.on('connection', function(socket){
+
+    console.log("Connection " + socket.id + " accepted.");
+    
+	// now that we have our connected 'socket' object, we can 
+    // define its event handlers
+
+    socket.on('disconnect', function(){
+        console.log("Connection " + socket.id + " terminated.");
+    });
+
+	// when we receive a message, send it to all connected clients
+    socket.on('message', function(message){
+		console.log("Received message: " + message + " - from client " + socket.id);
+		// relay that message to all connected clients
+		io.sockets.emit('chat', socket.id, message);
+	});
+
+});
+
